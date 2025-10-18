e853ee4134b171d943429b470c7fd5212757c20b
Ahmoham98 <electricallover@gmail.com>
2025-10-18 14:49:58 +0330

    -- update image path for the third time

--- src/components/InstallPWAModal.tsx
+++ src/components/InstallPWAModal.tsx
@@ -63,7 +63,7 @@ const InstallPWAModal: React.FC = () => {
             {/* App logo */}
             <div className="flex justify-center mb-4">
               <img
-                src="/icons/icon-192.png"
+                src="/icon-912.png"
                 alt="Mixinsalam Logo"
                 className="w-16 h-16 rounded-lg shadow-md"
               />
